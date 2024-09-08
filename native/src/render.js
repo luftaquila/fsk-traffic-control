731a4348bbd744ea8b9e235b8e1fac093d66df6d
오병준 <mail@luftaquila.io>
2024-09-09 07:45:28 +0900

    add: event listener function

--- native/src/render.js
+++ native/src/render.js
@@ -3,6 +3,32 @@ const { SerialPort } = require('serialport');
 
 let port;
 
+ipcRenderer.on('serial-open', (evt, data) => {
+  ipcRenderer.send('serial-request', '$HELLO');
+});
+
+// serial data received
+ipcRenderer.on('serial-data', (evt, data) => {
+  let str = String.fromCharCode(...data.data);
+  console.log(str)
+
+  // ignore non-protocol messages
+  if (str[0] !== "$") {
+    return;
+  }
+
+  if (str.includes("$HI")) {
+    ipcRenderer.send('notify', { title: `${str.substring(4)}번 컨트롤러 연결 완료 (${port.path})` });
+  }
+});
+
+function event_listeners() {
+  document.getElementById("connect").addEventListener("click", () => {
+    // TODO
+    ipcRenderer.send('serial-request', '$SENSOR 002 001 002');
+  });
+}
+
 async function list_ports() {
   for (let p of await SerialPort.list()) {
     switch (navigator.platform) {
@@ -24,30 +50,14 @@ async function list_ports() {
       }
     }
   }
+
   ipcRenderer.send('notify', {
     title: `장치 연결 실패 (장치 없음)`,
     message: "먼저 장치를 컴퓨터에 연결한 후에 프로그램을 실행하세요."
   });
+
   ipcRenderer.send('quit');
 }
 
-ipcRenderer.on('serial-open', (evt, data) => {
-  ipcRenderer.send('serial-request', '$HELLO');
-});
-
-// serial data received
-ipcRenderer.on('serial-data', (evt, data) => {
-  let str = String.fromCharCode(...data.data);
-
-  // ignore non-protocol messages
-  if (str[0] !== "$") {
-    return;
-  }
-
-  if (str.includes("$HI")) {
-    ipcRenderer.send('notify', { title: `${str.substring(4)}번 컨트롤러 연결 완료 (${port.path})` });
-  }
-});
-
-list_ports()
-
+event_listeners();
+list_ports();
